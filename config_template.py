c8d631442a37b223800d688783a948377acd28ac
Luca Zomparelli <luca.zomparelli@gmail.com>
2017-04-09 12:16:34 +0200

    Corretto errore nella chiave del dizionario 'api_keys' nel file config.

--- config_template.py
+++ config_template.py
@@ -1,5 +1,10 @@
-
+# Template da usare per il file di configurazione
+# in modo da evitare di scrivere utente e password
+# e apikey, nel repository di GitHub
+#
+# Per usarlo, farne una copia nominata config.py
+# nella cartella root dell'applicazione.
 
 api_keys = {
-	'telegrami': '<telegram_api_key>',
+	'telegram': '<telegram_api_key>',
 }
