88f67a716116c551a4d3a1a2c1feb3fe9f20ef16
Luca Zomparelli <luca.zomparelli@gmail.com>
2017-04-09 11:57:17 +0200

    tolta api key dal codice :.), e aggiunto al file ignore

--- telegram.py
+++ telegram.py
@@ -1,9 +1,10 @@
 import telepot
 import dataset
+from config import api_keys
 
 from time import sleep
 
-bot = telepot.Bot("379100520:AAF_-BKVmVmky2ZVDJIoeWAJNKtbDxWTVnY")
+bot = telepot.Bot(api_keys['telegram'])
 
 def handler(msg):
 	message = msg
